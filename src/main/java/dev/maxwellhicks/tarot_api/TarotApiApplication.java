e8edbba87457bc9e71f34e35cb6d2890db5f9bd1
Maxwell <skate1011rocks@gmail.com>
2026-08-03 16:17:36 -0600

    initial commit: generated Spring Boot project vis Initializr

---
+++ src/main/java/dev/maxwellhicks/tarot_api/TarotApiApplication.java
@@ -0,0 +1,13 @@
+package dev.maxwellhicks.tarot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotApiApplication.class, args);
+	}
+
+}
