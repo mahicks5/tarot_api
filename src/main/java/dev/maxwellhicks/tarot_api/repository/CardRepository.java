75eb4bdb4502c4ae3f3f48fd1bb8a1ed971b5913
Maxwell <skate1011rocks@gmail.com>
2026-08-28 14:42:09 -0600

    add CardRepository

---
+++ src/main/java/dev/maxwellhicks/tarot_api/repository/CardRepository.java
@@ -0,0 +1,7 @@
+package dev.maxwellhicks.tarot_api.repository;
+
+import dev.maxwellhicks.tarot_api.model.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
